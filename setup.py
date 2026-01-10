bc666f16ed5cf2f7188f664b73e00417cd09037d
Yasir Wali <yasirwali301302@gmail.com>
2026-01-10 15:57:37 +0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yasir Wali",
+    author_email="yasirwali3010302@gmail.com",
+    packages=find_packages()
+)
