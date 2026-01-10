719e282e6c693d7ed9beb0c4f6a1513818f5b60f
Yasir Wali <yasirwali301302@gmail.com>
2026-01-11 00:30:03 +0500

    pulled data from mongoDb with respect to clean train and test ingested files

--- demo.py
+++ demo.py
@@ -1,10 +1,15 @@
 
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
